5fca5778f1e48a11c7a78f65f994d936a5f49974
Tiffany Fust <tiffanyfust@gmail.com>
2026-02-18 22:13:52 -0800

    fixed form submits, navbar index page links, improved mobile layout

--- scripts.js
+++ scripts.js
@@ -56,3 +56,43 @@ const links = document.querySelectorAll('.nav-links a');
       link.classList.add('active');
     }
   });
+
+const forms = document.querySelectorAll('form');
+  
+  forms.forEach(form => {
+    const submitBtn = form.querySelector('button[type="submit"]');
+
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(form);
+      formData.append("access_key", "daa26cb7-31ac-41ea-b369-19718757b15a");
+
+      const originalText = submitBtn.textContent;
+
+      submitBtn.textContent = "Sending...";
+      submitBtn.disabled = true;
+
+      try {
+          const response = await fetch("https://api.web3forms.com/submit", {
+              method: "POST",
+              body: formData
+          });
+
+          const data = await response.json();
+
+          if (response.ok) {
+              alert("Success! Your message has been sent.");
+              form.reset();
+          } else {
+              alert("Error: " + data.message);
+          }
+
+      } catch (error) {
+          alert("Something went wrong. Please try again.");
+      } finally {
+          submitBtn.textContent = originalText;
+          submitBtn.disabled = false;
+      }
+  });
+})
