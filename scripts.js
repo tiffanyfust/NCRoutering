ba0ec339ea0f3b89dd64d9c70bcb606940c230c9
Tiffany Fust <tiffanyfust@gmail.com>
2026-02-25 01:06:43 -0800

    updated mobile sizing, edited mobile menu and js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,13 @@
-// Simple slider cycling
-let idx = 0;
-  const slider = document.querySelector('.hero-slider');
-  const slides = document.querySelectorAll('.slide');
-  const navButtons = document.querySelectorAll('.slider-nav button');
-  let interval = setInterval(nextSlide, 5000); // autoplay
 
-  function goToSlide(index) {
-    idx = index;
-    slider.style.transform = `translateX(-${idx * 100}%)`;
-    updateActiveButton();
-  }
+const mobileMenuIcon = document.querySelector('.mobileMenuIcon');
+const mobileMenuItems = document.querySelector('.hiddenMobileMenu');
 
-  function nextSlide() {
-    idx = (idx + 1) % slides.length;
-    goToSlide(idx);
-  }
+mobileMenuIcon.addEventListener('click', () => { 
+    if (window.innerWidth <= 1199 ) { 
+      console.log('Clicked while screen ≤ 500px');
+      mobileMenuItems.classList.toggle('open');
+  } });
 
-  function updateActiveButton() {
-    navButtons.forEach((btn, i) => {
-      btn.classList.toggle('active', i === idx);
-    });
-  }
-
-  // Add click events to buttons
-  navButtons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      const slideIndex = parseInt(btn.getAttribute('data-slide'), 10);
-      goToSlide(slideIndex);
-
-      // Optional: Reset autoplay when user clicks
-      clearInterval(interval);
-      interval = setInterval(nextSlide, 5000);
-    });
-  });
-
-  // Initial state
-  updateActiveButton();
 
 window.addEventListener('scroll', function() {
   const slide = document.querySelector('.slide');
@@ -43,19 +15,6 @@ window.addEventListener('scroll', function() {
   slide.style.backgroundPositionY = `${offset * 0.55}px`;
 });
 
-//checks active page 
-
-const links = document.querySelectorAll('.nav-links a');
-  const currentPath = window.location.pathname.split('/').pop(); // e.g. "about.html"
-
-  links.forEach(link => {
-    const href = link.getAttribute('href');
-
-    // Match exact filename or empty string (for index.html)
-    if (href === currentPath || (href === 'index.html' && currentPath === '')) {
-      link.classList.add('active');
-    }
-  });
 
 const forms = document.querySelectorAll('form');
   
